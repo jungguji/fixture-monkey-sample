bcdf50ce8bc9f9647f530272e0c8feb93de38fd3
JG.JI <junggu.ji.dev@gmail.com>
2024-12-16 11:23:52 +0900

    init project

---
+++ src/main/java/com/example/fixture_monkey/FixtureMonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.example.fixture_monkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixtureMonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixtureMonkeyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fixture_monkey/FixtureMonkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fixture_monkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixtureMonkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
